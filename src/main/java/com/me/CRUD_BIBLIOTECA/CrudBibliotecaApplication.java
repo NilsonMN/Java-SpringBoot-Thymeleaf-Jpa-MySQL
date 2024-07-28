ccdf3069c8f1d410f211c16ac9aa528e30c68983
nilson <nilsonjmn21@gmail.com>
2024-07-27 19:37:24 -0500

    java-springboot-thymeleaf-jpa-mysql

---
+++ src/main/java/com/me/CRUD_BIBLIOTECA/CrudBibliotecaApplication.java
@@ -0,0 +1,13 @@
+package com.me.CRUD_BIBLIOTECA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudBibliotecaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudBibliotecaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/me/CRUD_BIBLIOTECA/CrudBibliotecaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.me.CRUD_BIBLIOTECA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudBibliotecaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
